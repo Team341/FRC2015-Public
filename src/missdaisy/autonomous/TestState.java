5684c8dd49674f583605829306a0711ed5d29f2b
Joshua Sizer <joshsizer@gmail.com>
2017-01-01 17:01:49 -0500

    Add 2015 code to public repository

---
+++ src/missdaisy/autonomous/TestState.java
@@ -0,0 +1,28 @@
+package missdaisy.autonomous;
+
+public class TestState extends State {
+
+	public TestState(double blah, int blah2) {
+		super("TestState");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void running() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String parametersToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
